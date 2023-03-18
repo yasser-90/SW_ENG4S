e1a6cf4382488ec1d438225ea3a37e6a94926fdd
abdullahfawazS4A <Abdullah.cs476@student.uomosul.edu.iq>
2023-03-18 22:18:34 +0300

    first version

--- mainpro.py
+++ mainpro.py
@@ -1,5 +1,66 @@
- 
-file1 = open('C:\\Users\\HP\\Documents\\facttest.cpp', 'r')
-Lines = file1.readlines()
-for line in Lines:
-    print(line)
+def remove_comments(text):
+    """Removes both single and multi-line comments from the text"""
+
+    while True:
+
+        start =text.find('/*')
+        end = text.find('*/')
+        if start != -1 or end != -1:
+            text=text[:start]+text[end+2 :]
+        else:
+            break
+
+
+    while True:
+        start = text.find('//')
+        if start != -1:
+            i = start
+            while i < len(text):
+                if text[i] == '\n':
+                    end = i
+                    break
+                i += 1
+            text = text[:start] + text[end:]
+        else:
+            break
+
+    return text
+
+def remove_whitespace(text):
+    """Removes all whitespace characters from the text"""
+    return "".join(text.split())
+
+def remove_libraries(text):
+    """Removes all import statements from the text"""
+    while True:
+        start = text.find('#include')
+        if start != -1:
+            i = start
+            while i < len(text):
+                if text[i] == '\n':
+                    end = i
+                    break
+                i += 1
+            text = text[:start] + text[end:]
+        else:
+            break
+
+    return text
+# Read input file
+
+with open("test.cpp", "r") as file:
+    text = file.read()
+
+# Remove comments, whitespace, and libraries
+text = remove_comments(text)
+text = remove_libraries(text)
+text = remove_whitespace(text)
+
+
+
+
+with open("output.txt", "w") as file:
+    file.write(text)
+
+
+
---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+/*
+This is a multi-line comment
+It can span multiple lines
+*/
+
+
+//sdhfjhdsfjkhdjkfhjdhfjh
+int main()
+{
+    std::cout << "Enter two numbers: ";
+    double x, y;
+    //skdfkldsjfkldjsfklj
+    std::cin >> x >> y;
+    double result = std::pow(x, y);
+    std::cout << x << "^" << y << " = " << result << std::endl;
+    return EXIT_SUCCESS;
+}
