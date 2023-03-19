c293f27399e58708ccb0d0bcab398d591089e957
unknown <abdulrhmanrady0101@gmail.com>
2023-03-19 22:12:14 +0300

    Functions updated

--- mainpro.py
+++ mainpro.py
@@ -1,5 +1,39 @@
- 
-file1 = open('C:\\Users\\HP\\Documents\\facttest.cpp', 'r')
-Lines = file1.readlines()
-for line in Lines:
-    print(line)
+class Functions():
+    def remove_Spaces(file_path):
+        with open(file_path, 'r') as file:
+            content = file.readlines()
+        stripped_lines=""
+        for line in content:
+            for l in line:
+                if l ==" " or l=="\n":
+                    l=""
+
+                else:
+                    stripped_lines+=l
+        print(f"remove Spaces:\n{stripped_lines}")
+
+    def remove_Libraries(file_path):
+        with open(file_path, 'r') as file:
+            content = file.readlines()
+
+        stripped_lines = ""
+        for line in content:
+            if not line.startswith('#include'):
+                stripped_lines+= line
+        print(f"remove Libraries:\n{stripped_lines}")
+        # Join the stripped lines and write them back to the file
+        
+    def remove_Comments(file_path):
+        with open(file_path, 'r') as file:
+            content = file.readlines()
+        stripped_lines = ""
+        for line in content:
+            if not line.startswith('/*') and not line.endswith('*/') and not line.startswith('//'):
+                stripped_lines+= line
+        print(f"remove Comments:\n{stripped_lines}")
+
+
+# test class functions
+Functions.remove_Libraries("C:\\Users\DELL\Desktop\mat2.txt")
+Functions.remove_Comments("C:\\Users\DELL\Desktop\mat2.txt")
+Functions.remove_Spaces("C:\\Users\DELL\Desktop\mat2.txt")
