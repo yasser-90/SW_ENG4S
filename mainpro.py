ceca4796bea96a7fb50c69503308f1d117741d56
shefaa <your email@SAMLECOM>
2023-03-24 12:34:29 +0300

    shefaa&aya&nagham

--- mainpro.py
+++ mainpro.py
@@ -1,5 +1,32 @@
- 
-file1 = open('C:\\Users\\HP\\Documents\\facttest.cpp', 'r')
+def save(text):
+    savee=open("C:\\Users\\ALMANAR\\Desktop\\new.txt",'a')
+    savee.write(text)
+    savee.close()
+
+file1 = open("C:\\Users\\ALMANAR\\Desktop\\file.txt", 'r')
 Lines = file1.readlines()
+x=''
 for line in Lines:
-    print(line)
+   a=line.find('function')
+   print(a)
+   a2 = line.replace("function", '')
+
+   slash = line.find('//')
+   print(slash)
+   #{print('//  in =>' + slash)}
+   a2=a2.replace("//", '')
+   if(slash>=0):
+        a2 = a2.replace(a2,'')
+
+   if (len(line)>1):
+       save(a2)
+  # print(line)
+
+   print(a2)
+   x=a2
+
+
+
+
+
+
