b651552da2dd1227ce7d19186b42b2870162a5e8
WalaaAhmadS4B <126968402+WalaaAhmadS4B@users.noreply.github.com>
2023-03-25 23:07:51 +0300

    Add files via upload

--- mainpro.py
+++ mainpro.py
@@ -1,40 +1,37 @@
- 
-file1 = open('file.cpp', 'r')
-Lines = file1.readlines()
-for line in Lines:
-    
-    def Remove(line):
-     return line.replace(" ", "")
- 
-def RemoveComments(line, text):
-        result = text
-        ofs = 0
-        if text:
-            lines = text.split('\*' ,'*/','//')
-            if lines.strip() == '/*': # remove comment
-                ofs = -1
-            elif  lines.strip() == '/*': # remove comment
-                ofs = -1
-            elif  lines.strip() == '*/': # remove comment
-                   ofs = -1
-            elif  lines.strip() == '//': # remove comment
-                      ofs = -1
-        else:
-          ofs=1
-          return result, ofs
-        
-def remove(line):
-          if "#" in contents[line]:
-
-        # delete the line if startswith "#"
-            if contents[line].startswith("#"):
-               contents.remove(contents[line])
-               line+=1
-
-          
-          
-
-print(remove(line))
+def remove_spaces():
+    """
+    Removes all spaces from the input code.
+    """
+    file1 = open('code.txt', 'r')
+    for line in file1.readlines():
+        line = line.replace(' ', '')
+        # write line in file
+        print(line)
 
 
-print(line)
+def remove_libraries():
+    """
+    Removes all libraries from the input code.
+    """
+    file1 = open('code.txt', 'r')
+    for line in file1.readlines():
+        if line.find('#include'):
+            # write line in file
+            print(line)
+
+
+def remove_comments():
+    """
+    Removes all comments from the input code.
+    """
+    file1 = open('code.txt', 'r')
+    for line in file1.readlines():
+        if line.find('//') or line.find('*/') or line.find('/*'):
+            # write line in file
+            print(line)
+
+
+x= remove_spaces()
+y= remove_libraries()
+z= remove_comments()
+
