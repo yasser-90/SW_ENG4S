b84a2bb0e03b538fc31ce6f4fbbf3ad2388cde48
AsmaaALsaqerS4B <126966703+AsmaalsaqerS4B@users.noreply.github.com>
2023-03-25 21:31:45 +0300

    Add files via upload

---
+++ SW_ENG4S/README.md
@@ -0,0 +1,7 @@
+# SW_ENG4S
+this project read files and remove all not important parts from this files
+# files read by this project written using C++ language ( code )
+# you must add file contain 3 functions 
+# first function Remove all comments (single or block comment )from file
+# second function remove all whitespace from file
+# third function remove all Call external libraries
---
+++ SW_ENG4S/mainpro.py
@@ -0,0 +1,37 @@
+def remove_spaces():
+    """
+    Removes all spaces from the input code.
+    """
+    file1 = open('code.txt', 'r')
+    for line in file1.readlines():
+        line = line.replace(' ', '')
+        # write line in file
+        print(line)
+
+
+def remove_libraries():
+    """
+    Removes all libraries from the input code.
+    """
+    file1 = open('code.txt', 'r')
+    for line in file1.readlines():
+        if line.find('#include'):
+            # write line in file
+            print(line)
+
+
+def remove_comments():
+    """
+    Removes all comments from the input code.
+    """
+    file1 = open('code.txt', 'r')
+    for line in file1.readlines():
+        if line.find('//') or line.find('*/') or line.find('/*'):
+            # write line in file
+            print(line)
+
+
+x= remove_spaces()
+y= remove_libraries()
+z= remove_comments()
+
