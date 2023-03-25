67b917a0d937e3230feb2a6234b1e13642716fb6
Zahraa AliS4B <126602166+zahraa-Ali-Mohammed@users.noreply.github.com>
2023-03-25 22:42:54 +0300

    Update mainpro.py

--- mainpro.py
+++ mainpro.py
@@ -1,6 +1,40 @@
  
-file1 = open('yasser-90/SW_ENG4S/file.cpp', 'r')
+file1 = open('file.cpp', 'r')
 Lines = file1.readlines()
 for line in Lines:
-    print(line)
-print(Lines)
+    
+    def Remove(line):
+     return line.replace(" ", "")
+ 
+def RemoveComments(line, text):
+        result = text
+        ofs = 0
+        if text:
+            lines = text.split('\*' ,'*/','//')
+            if lines.strip() == '/*': # remove comment
+                ofs = -1
+            elif  lines.strip() == '/*': # remove comment
+                ofs = -1
+            elif  lines.strip() == '*/': # remove comment
+                   ofs = -1
+            elif  lines.strip() == '//': # remove comment
+                      ofs = -1
+        else:
+          ofs=1
+          return result, ofs
+        
+def remove(line):
+          if "#" in contents[line]:
+
+        # delete the line if startswith "#"
+            if contents[line].startswith("#"):
+               contents.remove(contents[line])
+               line+=1
+
+          
+          
+
+print(remove(line))
+
+
+print(line)
